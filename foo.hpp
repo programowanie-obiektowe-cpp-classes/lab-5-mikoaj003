fe4507e436d7da26119206b2e8cad3f82c78c94e
Mikołaj Urbański <mikiurb56@gmail.com>
2025-11-17 23:10:18 +0100

    .

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,24 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > vektor(people.size(), 0);
+    char                y = 'y';
+    char                n = 'n';
+    int                 index = 0;
+
+
+    for (auto it = people.begin(); it != people.end(); ++it, ++index) {
+        
+        // age++
+        it->birthday();
+
+        if (it->isMonster() == true) {
+            vektor[people.size() - index - 1] = n;
+        }
+        if (it->isMonster() == false) {
+            vektor[people.size() - index - 1] = y;
+        }
+    }
+
+    return { vektor};
 }
